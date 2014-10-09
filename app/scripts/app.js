de4ba64d43e685cda2822de8fe47416e7213eccd
Benjamin Marimon <bsmarimon@berkeley.edu>
2014-10-09 15:04:45 -0700

    redid first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name finalApp
+ * @description
+ * # finalApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('finalApp', [
+      'ui.bootstrap'
+  ]);
